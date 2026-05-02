ff4e75e123e9eb6cc54f677b8b63be3cbfc48352
Mingbo Lu <lumb0409@gmail.com>
2026-05-02 16:35:31 +0800

    feat(mybatis-plus): 为mybatis-plus提供字段填充策略

---
+++ base/src/main/java/github/lukingyu/shortlink/base/config/MyMetaObjectHandler.java
@@ -0,0 +1,25 @@
+package github.lukingyu.shortlink.base.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * 为 MyBatis Plus 自动配置 提供一个Bean，声明字段填充时的策略。使 fill 注解属性生效
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        strictUpdateFill(metaObject, "updateTime", Date::new, Date.class);
+    }
+}
