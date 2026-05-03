e862b523bf8c66ac977d8ed5af61559d3d9471fc
Mingbo Lu <lumb0409@gmail.com>
2026-05-03 19:48:15 +0800

    feat(project): project分页接口返回时间格式化
    - 还有额外一些细节修改

--- base/src/main/java/github/lukingyu/shortlink/base/entity/dto/resp/ShortLinkPageRespDTO.java
+++ base/src/main/java/github/lukingyu/shortlink/base/entity/dto/resp/ShortLinkPageRespDTO.java
@@ -1,5 +1,6 @@
 package github.lukingyu.shortlink.base.entity.dto.resp;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -45,7 +46,14 @@ public class ShortLinkPageRespDTO {
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
 
     /**
      * 描述
