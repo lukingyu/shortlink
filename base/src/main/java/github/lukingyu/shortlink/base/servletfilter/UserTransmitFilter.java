d93223e23832da3ab5365dd07791fc0b589b0542
Mingbo Lu <lumb0409@gmail.com>
2026-05-03 16:13:20 +0800

    feat(user): admin用户信息过滤器返回友好提示

--- base/src/main/java/github/lukingyu/shortlink/base/servletfilter/UserTransmitFilter.java
+++ base/src/main/java/github/lukingyu/shortlink/base/servletfilter/UserTransmitFilter.java
@@ -1,15 +1,23 @@
 package github.lukingyu.shortlink.base.servletfilter;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
+import com.google.common.collect.Lists;
 import github.lukingyu.shortlink.base.biz.user.UserContext;
 import github.lukingyu.shortlink.base.biz.user.UserInfoDTO;
 import github.lukingyu.shortlink.base.constant.RedisCacheConstant;
+import github.lukingyu.shortlink.base.entity.enums.UserErrorCodeEnum;
+import github.lukingyu.shortlink.base.entity.exception.ClientException;
+import github.lukingyu.shortlink.base.entity.result.Results;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 import java.util.Objects;
 
 @RequiredArgsConstructor
@@ -17,15 +25,34 @@ public class UserTransmitFilter implements Filter {
 
     private final StringRedisTemplate stringRedisTemplate;
 
+    private static final List<String> IGNORE_URI = Lists.newArrayList(
+            "/api/short-link/admin/v1/user/login",
+            "/api/short-link/admin/admin/v1/actual/user/has-username"
+    );
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String requestURI = httpServletRequest.getRequestURI();
-        if (!Objects.equals(requestURI, "/api/short-link/v1/user/login")) {
-            String username = httpServletRequest.getHeader("username");
-            String token = httpServletRequest.getHeader("token");
-            Object userInfoJsonStr = stringRedisTemplate.opsForHash().get(RedisCacheConstant.LOGIN_USER_TOKEN_KEY + username, token);
-            if (userInfoJsonStr != null) {
+        if (!IGNORE_URI.contains(requestURI)) {
+            String method = httpServletRequest.getMethod();
+            if (!(Objects.equals(requestURI, "/api/short-link/admin/v1/user") && Objects.equals(method, "POST"))) {
+                String username = httpServletRequest.getHeader("username");
+                String token = httpServletRequest.getHeader("token");
+                if (!StrUtil.isAllNotBlank(username, token)) {
+                    writeJson((HttpServletResponse) servletResponse, JSON.toJSONString(Results.failure(new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL))));
+                    return;
+                }
+                Object userInfoJsonStr;
+                try {
+                    userInfoJsonStr = stringRedisTemplate.opsForHash().get(RedisCacheConstant.LOGIN_USER_TOKEN_KEY + username, token);
+                    if (userInfoJsonStr == null) {
+                        throw new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL);
+                    }
+                } catch (Exception ex) {
+                    writeJson((HttpServletResponse) servletResponse,  JSON.toJSONString(Results.failure(new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL))));
+                    return;
+                }
                 UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
                 UserContext.setUser(userInfoDTO);
             }
@@ -35,6 +62,15 @@ public class UserTransmitFilter implements Filter {
             filterChain.doFilter(servletRequest, servletResponse);
         } finally {
             UserContext.removeUser();
+        }
+    }
+
+    private void writeJson(HttpServletResponse response, String json) {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=utf-8");
+        try (PrintWriter writer = response.getWriter()) {
+            writer.print(json);
+        } catch (IOException ignore) {
         }
     }
 }
